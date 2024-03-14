8bae24f2355b76cfeb97df1bdc9e3df754354ad9
serhiifnn <163458734+serhiifnn@users.noreply.github.com>
2024-03-14 19:55:54 +0000

    second code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+using  namespace std;
+
 int main() {
-  std::cout << "Hello World!\n";
+  const int size = 5;
+
+  int array[size] = {1, 2, 3, 4, 5};
+
+  cout << "Елементи Масиву:" << endl;
+  for(int i = 0; i < size; ++i) {
+    cout << array[i] << " ";
+  }
+  cout << endl;
+
+  return 0;
 }
